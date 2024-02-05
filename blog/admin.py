99128d8b024bc1139349ec269df98265629a26dc
Herbert-Haase <herberthaase02@gmx.de>
2024-02-05 19:05:49 +0100

    restructure models

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import Blog, Comment, Author
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Blog)
+admin.site.register(Comment)
+admin.site.register(Author)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse  # To generate URLS by reversing URL patterns
+import uuid  # Required for unique book instances
 # Create your models here.
 
 class Blog(models.Model):
@@ -12,6 +13,10 @@ class Blog(models.Model):
     
     class Meta:
         ordering = ['date']
+    
+    def __str__(self):
+            """String for representing the Model object."""
+            return f'{self.title}'
 
 class Comment(models.Model):
     """Model representing a comment under a blog post written by a logged in user"""
@@ -20,17 +25,28 @@ class Comment(models.Model):
     blog = models.ForeignKey(
         'Blog', on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
-    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular comment across blogs")
     class Meta:
         ordering = ['date']
 
+    def __str__(self):
+            """String for representing the Model object."""
+            return f'{self.text[:10]}, {self.blog}'
 
 class User(AbstractUser):
     """Model representing a logged in User who can comment"""
     name = models.CharField(max_length=200, unique=True)
+    
+    def __str__(self):
+            """String for representing the Model object."""
+            return f'{self.name}'
 
 class Author(models.Model):
-    """Model representing a logged in User who can write blogs"""
+    """Model representing a logged in User, who can write blogs"""
     name = models.ForeignKey('User', on_delete=models.CASCADE)
     bioinformation = models.TextField(max_length=1000, help_text="Enter a brief description about yourself")
 
+    def __str__(self):
+            """String for representing the Model object."""
+            return f'{self.name}'
